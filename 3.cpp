ea23c94060f3252e363f174905d8497a4c5c1ce8
SharkAttack48 <kuzmin2013@yandex.ru>
2017-04-13 20:27:41 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+using namespace std;
+class people
+{
+	private:
+		char* fio;
+		unsigned exp, sal;
+	public:
+		people(){fio=NULL;}
+		people(const people&);
+		people(char*,unsigned,unsigned);
+		~people(){delete [] fio;}
+		people& operator=(const people&);
+		friend ostream& operator<<(ostream&,const people&);
+		friend istream& operator>>(istream&,people&);
+		char* getfio(){return fio;}
+		unsigned getexp(){return exp;}
+		unsigned getsal(){return sal;}
+};
+
+people::people(const people&p)
+{
+	fio=new char[strlen(p.fio)+1];
+	strcpy(fio,p.fio);
+	exp=p.exp;sal=p.sal;
+}
+
+people::people(char* afio, unsigned aexp, unsigned asal)
+{
+	fio=new char[strlen(afio)+1];
+	strcpy(fio,afio);
+	exp=aexp;sal=asal;
+}
+
+people& people::operator=(const people& p)
+{
+	if (this!=&p)
+	{
+		delete[]fio;
+		fio=new char[strlen(p.fio)+1];
+		strcpy(fio,p.fio);
+		exp=p.exp;sal=p.sal;
+	}
+	return *this;
+}
+
+ostream& operator<<(ostream& s,const people& p)
+{
+	s<<p.fio<<" "<<p.exp<<" "<<p.sal<<"\n";
+	return s;
+}
+
+istream& operator>>(istream& s,people& p)
+{
+	char buf[100];
+	s>>buf>>p.exp>>p.sal;
+	delete[]p.fio;
+	p.fio=new char[strlen(buf)+1];
+	strcpy(p.fio,buf);
+	return s;
+}
+
+int main()
+{
+	people p[10];
+	char name[30];
+	int i,n,s;
+	unsigned sl,xp;
+	cin>>n;
+	for (i=0;i<n;i++) cin>>p[i];
+	cout<<'\n';
+	for (i=0;i<n;i++) cout<<p[i];
+	cout<<'\n';
+	unsigned maxsl=p[0].getsal();
+	char*maxslfio;
+	maxslfio=new char[strlen(p[0].getfio())+1];
+	strcpy(maxslfio,p[0].getfio());
+	for (i=1;i<n;i++)
+		if ((p[i].getsal()>maxsl) && (p[i].getexp()<10))
+		{
+			maxsl=p[i].getsal();
+			strcpy(maxslfio,p[i].getfio());
+		}
+	cout<<"man with exp < 10 & maxsl:"<<maxslfio;
+	cout<<'\n';
+	p[4]=p[2];
+	cout<<"operator= test "<< p[4];
+}
---
+++ 5.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+using namespace std;
+
+class people
+{
+	protected:
+		char* fio;
+		unsigned exp, sal;
+	public:
+		people(){fio=NULL;}
+		people(const people&);
+		people(char*,unsigned,unsigned);
+		~people(){delete [] fio;}
+		people& operator=(const people&);
+		friend ostream& operator<<(ostream&,const people&);
+		friend istream& operator>>(istream&,people&);
+		char* getfio(){return fio;}
+		unsigned getexp(){return exp;}
+		unsigned getsal(){return sal;}
+		void print(){cout<<"Name:"<<fio<<" exp:"<<exp<<" sal:"<<sal;}
+};
+
+people::people(const people&p)
+{
+	fio=new char[strlen(p.fio)+1];
+	strcpy(fio,p.fio);
+	exp=p.exp;sal=p.sal;
+}
+
+people::people(char* afio, unsigned aexp, unsigned asal)
+{
+	fio=new char[strlen(afio)+1];
+	strcpy(fio,afio);
+	exp=aexp;sal=asal;
+}
+
+people& people::operator=(const people& p)
+{
+	if (this!=&p)
+	{
+		delete[]fio;
+		fio=new char[strlen(p.fio)+1];
+		strcpy(fio,p.fio);
+		exp=p.exp;sal=p.sal;
+	}
+	return *this;
+}
+
+ostream& operator<<(ostream& s,const people& p)
+{
+	s<<p.fio<<" "<<p.exp<<" "<<p.sal;
+	return s;
+}
+
+istream& operator>>(istream& s,people& p)
+{
+	char buf[100];
+	s>>buf>>p.exp>>p.sal;
+	delete[]p.fio;
+	p.fio=new char[strlen(buf)+1];
+	strcpy(p.fio,buf);
+	return s;
+}
+
+class g_people:public people
+{
+	char* gender;
+	public:
+		g_people():people(){gender=NULL;}
+		g_people(const g_people&);
+		g_people(const g_people&,char*);
+		g_people(const people&,char*);
+		g_people(char*,unsigned,unsigned,char*);
+		~g_people(){delete [] gender;}
+		char* getgender(){return gender;}
+		void print(){people::print();cout<<" "<<gender;}
+		g_people& operator=(const g_people&);
+		friend ostream& operator<<(ostream&,const g_people&);
+		friend istream& operator>>(istream&,g_people&);
+};
+
+g_people::g_people(char*afio,unsigned aexp,unsigned asal,char*agender):people(afio,aexp,asal)
+{
+	gender=new char[strlen(agender)+1];
+	strcpy(gender,agender);
+}
+
+g_people::g_people(const people& p,char* agender):people(p)
+{
+	gender=new char[strlen(agender)+1];
+	strcpy(gender,agender);
+}
+
+g_people::g_people(const g_people& p,char* agender):people(p)
+{
+	gender=new char[strlen(agender)+1];
+	strcpy(gender,agender);
+}
+
+ostream&operator<<(ostream& s,const g_people& c)
+{
+	s<<people(c)<<" "<<c.gender<<"\n";
+	return s;
+}
+
+istream&operator>>(istream& s,g_people& c)
+{
+	char buf[100];
+	people x;
+	c.~g_people();
+	s>>x>>buf;
+	c=g_people(x,buf);
+	return s;
+}
+
+g_people&g_people::operator=(const g_people& p)
+{
+	if (&p!=this)
+	{
+		this->~g_people();
+		fio=new char[strlen(p.fio)+1];strcpy(fio,p.fio);
+		gender=new char[strlen(p.gender)+1];strcpy(gender,p.gender);
+		exp=p.exp;sal=p.sal;		
+	}
+	return *this;
+}
+
+int main()
+{
+	g_people p[10];
+	char name[30];
+	int i,n,s;
+	unsigned sl,xp;
+	char t[]="w";
+	cin>>n;
+	for (i=0;i<n;i++) cin>>p[i];
+	cout<<'\n';
+	for (i=0;i<n;i++) cout<<p[i];
+	cout<<'\n';
+	unsigned maxsl=p[0].getsal();
+	char*maxslfio;
+	maxslfio=new char[strlen(p[0].getfio())+1];
+	strcpy(maxslfio,p[0].getfio());
+	for (i=1;i<n;i++)
+		if ((p[i].getsal()>maxsl)&&(p[i].getexp()<10)&&(!strcmp(p[i].getgender(),t)))
+		{
+			maxsl=p[i].getsal();
+			strcpy(maxslfio,p[i].getfio());
+		}
+	cout<<"woman with exp < 10 & maxsl:"<<maxslfio;
+	cout<<'\n';
+	p[n+1]=p[1];
+	cout<<"operator= test "<< p[n+1];
+}
