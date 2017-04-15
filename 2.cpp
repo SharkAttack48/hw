039a97b027a52d02503be05e544c6fa2d73e96b1
SharkAttack48 <kuzmin2013@yandex.ru>
2017-04-15 05:13:38 +0400

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <cstring>
+
 using namespace std;
+
 class people
 {
 	private:
 		char* fio;
 		unsigned exp, sal;
 	public:
-		people (){fio=NULL;}
-		people (char*,unsigned,unsigned);
-		~people (){delete [] fio;}
+		people(){fio=NULL;}
+		people(char*,unsigned,unsigned);
+		~people(){delete [] fio;}
 		char* getfio(){return fio;}
 		unsigned getexp(){return exp;}
 		unsigned getsal(){return sal;}
@@ -19,22 +21,16 @@ class people
 
 people::people(char* afio, unsigned aexp, unsigned asal)
 {
-	int n;
-	n=strlen(afio);
-	fio=new char[n+1];
+	fio=new char[strlen(afio)+1];
 	strcpy(fio,afio);
-	exp=aexp;
-	sal=asal;
+	exp=aexp;sal=asal;
 }
 
 people::people(const people&p)
 {
-	int n;
-	n=strlen(p.fio);
-	fio=new char[n+1];
+	fio=new char[strlen(p.fio)+1];
 	strcpy(fio,p.fio);
-	exp=p.exp;
-	sal=p.sal;
+	exp=p.exp;sal=p.sal;
 }
 
 void people::print()
@@ -49,26 +45,34 @@ int main()
 	char name[30];
 	int i,n,s;
 	unsigned sl,xp;
+	cout<<"Enter number of employers: ";
 	cin>>n;
+	cout<<"Enter data (name|experience|salary) of employers: ";
 	for (i=0;i<n;i++)
 	{
 		cin >>name >>xp >>sl;
 		p[i]=new people(name,xp,sl);
 	}
-	cout<<'\n';
+	cout<<"\n";
 	for (i=0;i<n;i++) p[i]->print();
-	cout<<'\n';
-	unsigned maxsl=p[0]->getsal();
+	cout<<"\n";
+	for (i=0;i<n;i++)
+		if(p[i]->getexp()<10)
+		{		
+			s=i;
+			break;
+		}
+	unsigned maxsl=p[s]->getsal();
 	char*maxslfio;
-	maxslfio=new char[strlen(p[0]->getfio())+1];
-	strcpy(maxslfio,p[0]->getfio());
-	for (i=1;i<n;i++)
+	maxslfio=new char[strlen(p[s]->getfio())+1];
+	strcpy(maxslfio,p[s]->getfio());
+	for (i=s+1;i<n;i++)
 		if ((p[i]->getsal()>maxsl) && (p[i]->getexp()<10))
 		{
 			maxsl=p[i]->getsal();
 			strcpy(maxslfio,p[i]->getfio());
 		}
-	cout<<"man with exp < 10 & maxsl:"<<maxslfio;
+	cout<<"man with experince <10 and max salary: "<<maxslfio;
 	cout<<'\n';
 	for (i=0;i<n;i++) delete p[i];
 }
